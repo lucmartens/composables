e52c1d8fcdf8a8f0f5778b6744fd64faf630b277
Luc Martens <luc.martens@greenhousegroup.com>
2018-01-09 18:59:52 +0100

    Add Hoc component

---
+++ src/__tests__/hoc.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import Hoc from '../hoc';
+
+describe('Hoc', () => {
+  const hoc = n => Component => <Component n={n + 1} />;
+
+  test('applies a higher order component', () => {
+    const apply = jest.fn(() => hoc(0));
+    const render = jest.fn(() => null);
+    mount(<Hoc apply={apply} render={render} test="value" />);
+
+    expect(apply.mock.calls.length).toBe(1);
+    expect(apply.mock.calls[0][0]).toMatchSnapshot();
+  });
+
+  test('calls render with the resulting props', () => {
+    const apply = jest.fn(() => hoc(0));
+    const render = jest.fn(() => null);
+    mount(<Hoc apply={apply} render={render} />);
+
+    expect(render.mock.calls.length).toBe(1);
+    expect(render.mock.calls[0][0]).toMatchSnapshot();
+  });
+
+  test('does not re-apply higher order component on prop changes', () => {
+    const apply = jest.fn(() => hoc(0));
+    const render = jest.fn(() => null);
+    const wrapper = mount(<Hoc apply={apply} render={render} />);
+
+    expect(apply.mock.calls.length).toBe(1);
+    wrapper.setProps({ test: 'value' });
+    expect(apply.mock.calls.length).toBe(1);
+    wrapper.setProps({ hoc: () => {} });
+    expect(apply.mock.calls.length).toBe(1);
+  });
+});
---
+++ src/hoc.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import With from './with';
+
+const PassThrough = ({ render, ...props }) => render(props);
+
+const Hoc = ({ apply, render, ...props }) => (
+  <With
+    shouldUpdate={() => false}
+    enter={() => apply(props)(PassThrough)}
+    render={el => React.cloneElement(el, { render })}
+  />
+);
+
+Hoc.propTypes = {
+  apply: PropTypes.func.isRequired,
+  render: PropTypes.func.isRequired
+};
+
+export default Hoc;
