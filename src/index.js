8cedbdb12806b1939bd90fbd0d4601b30fc99ee8
Luc Martens <luc.martens@greenhousegroup.com>
2018-01-16 20:58:47 +0100

    Reduce public API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,2 @@
-export { default as Bind } from './bind';
-export { default as Event } from './event';
-export { default as Hoc } from './hoc';
 export { default as Result } from './result';
-export { default as State } from './state';
 export { default as With } from './with';
